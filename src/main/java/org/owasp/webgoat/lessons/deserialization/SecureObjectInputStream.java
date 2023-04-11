b8874e5f2b585aea9b876c6972b4316bf0075274
elady56 <elady02@gmail.com>
2023-04-11 19:51:10 +0200

    test deserialization

--- src/main/java/org/owasp/webgoat/lessons/deserialization/SecureObjectInputStream.java
+++ src/main/java/org/owasp/webgoat/lessons/deserialization/SecureObjectInputStream.java
@@ -1,5 +1,10 @@
 package org.owasp.webgoat.lessons.deserialization;
 
+import org.owasp.webgoat.container.lessons.Hint;
+import org.owasp.webgoat.container.session.Course;
+import org.owasp.webgoat.lessons.challenges.Email;
+import org.owasp.webgoat.lessons.challenges.Flag;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,14 +19,13 @@ public class SecureObjectInputStream extends ObjectInputStream {
     protected Class<?> resolveClass(ObjectStreamClass osc) throws IOException, ClassNotFoundException {
 
         List<String> approvedClasses = new ArrayList<String>();
-        //approvedClasses.add(AllowedClass1.class.getName());
-        //approvedClasses.add(AllowedClass2.class.getName());
-        approvedClasses.add("1");
+        approvedClasses.add(Course.class.getName());
+        approvedClasses.add(Hint.class.getName());
+        approvedClasses.add(Email.class.getName());
+        approvedClasses.add(Flag.class.getName());
         if (!approvedClasses.contains(osc.getName())) {
             throw new InvalidClassException("Unauthorized deserialization", osc.getName());
         }
-
-
         return super.resolveClass(osc);
     }
 }
